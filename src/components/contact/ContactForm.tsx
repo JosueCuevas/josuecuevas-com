aad1c79261a6efb4b68b393cb0f93909449bc5e4
Josue Cuevas <josue19cuevas99@gmail.com>
2023-06-01 23:00:43 -0600

    fixing form

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -21,6 +21,7 @@ const ContactForm: React.FC = () => {
           </label>
           <input
             type="text"
+            name="Nombre"
             id="first_name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-60"
             placeholder="Escribe tu nombre..."
@@ -38,6 +39,7 @@ const ContactForm: React.FC = () => {
         <input
           type="email"
           id="email"
+          name="email"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           placeholder="hola@ejemplo.com"
           required
@@ -52,6 +54,7 @@ const ContactForm: React.FC = () => {
       </label>
       <textarea
         id="message"
+        name="comentarios"
         rows={4}
         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-50"
         placeholder="Escribe tus comentarios..."
